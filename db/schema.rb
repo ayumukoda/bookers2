38b07250b4fae30272f5538cd6e8e7f8edb0e723
ayumukoda <168876414+ayumukoda@users.noreply.github.com>
2024-07-12 14:13:56 +0000

    ログイン、ログアウト後の遷移先設定

--- db/schema.rb
+++ db/schema.rb
@@ -42,7 +42,8 @@ ActiveRecord::Schema.define(version: 2024_07_05_220628) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "body"
+    t.text "body"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
